2065b782ab9d8a4cc42c8f7c018d5df4e2f6f472
Ekaterina <brekhova.katya@gmail.com>
2022-04-05 17:23:20 +0300

    Refactor gendiff algorithp and stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,15 +3,38 @@ import isObject from '../utilitys.js';
 const stylishValue = (data, level) => {
   if (isObject(data)) {
     const entries = Object.entries(data);
-    return `{\n${entries.map(([key, value]) => `${' '.repeat(level + 6)}${key}: ${stylishValue(value, level + 4)}`).join('\n')}\n${' '.repeat(level + 2)}}`;
+    return `{\n${entries.map(([key, value]) => `${' '.repeat(level + 2)}${key}: ${stylishValue(value, level + 4)}`).join('\n')}\n${' '.repeat(level - 2)}}`;
   }
   return data;
 };
 
-const stylish = (params) => params
-  .map(({
-    key, value, level, char,
-  }) => (`${' '.repeat(level)}${char} ${key}: ${stylishValue(value, level)}`))
-  .join('\n');
+const stylishLine = ({
+  key, from, to, level, status,
+}) => {
+  switch (status) {
+    case 'added':
+      return `${' '.repeat(level)}+ ${key}: ${stylishValue(to, level + 4)}\n`;
+    case 'removed':
+      return `${' '.repeat(level)}- ${key}: ${stylishValue(from, level + 4)}\n`;
+    case 'updated':
+      return `${' '.repeat(level)}- ${key}: ${stylishValue(from, level + 4)}\n${' '.repeat(level)}+ ${key}: ${stylishValue(to, level + 4)}\n`;
+    case 'not-touched':
+      return `${' '.repeat(level)}  ${key}: ${stylishValue(from, level + 4)}\n`;
+    default:
+      return `${' '.repeat(level)}  ${key}: ${stylishValue(from, level + 4)}\n`;
+  }
+};
+
+const stylish = (params) => {
+  const inner = (node) => {
+    if (node.status === 'root') {
+      return `${' '.repeat(node.level + 2)}${node.key}: {\n${node.value.flatMap(inner).join('')}${' '.repeat(node.level + 2)}}\n`;
+    }
+    return stylishLine(node);
+  };
+  const res = params.flatMap(inner).join('');
+
+  return `{\n${res}}`;
+};
 
 export default stylish;
--- src/index.js
+++ src/index.js
@@ -22,59 +22,56 @@ const format = (formatName, params) => {
   switch (formatName) {
     case 'stylish':
       return stylish(params);
+
     case 'plain':
       return plain(params);
+
     default: return '';
   }
 };
 
-const genLineDiff = (key, data1, data2, level) => {
+const getLineDiff = (key, data1, data2) => {
   if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
-    return [{
-      key, value: data2[key], char: '+', level,
-    }];
+    return {
+      key, from: null, to: data2[key], status: 'added',
+    };
   }
   if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
-    return [{
-      key, value: data1[key], char: '-', level,
-    }];
+    return {
+      key, from: data1[key], to: null, status: 'removed',
+    };
   }
   if (data1[key] !== data2[key]) {
-    return [{
-      key, value: data1[key], char: '-', level,
-    }, {
-      key, value: data2[key], char: '+', level,
-    }];
+    return {
+      key, from: data1[key], to: data2[key], status: 'updated',
+    };
   }
-  return [{
-    key, value: data1[key], char: ' ', level,
-  }];
+  return {
+    key, from: data1[key], to: data2[key], status: 'not-touched',
+  };
 };
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = readFile(filepath1);
   const data2 = readFile(filepath2);
 
-  const compareData = (value1, value2, level = 2) => {
+  const compareData = (value1, value2, level = 2, way = []) => {
     const set = new Set([...Object.keys(value1), ...Object.keys(value2)]);
     const allKeys = [...set].sort();
 
-    const diffArr = allKeys.map((key) => {
+    const diffParams = allKeys.map((key) => {
       if (isObject(value1[key]) && isObject(value2[key])) {
-        const value = compareData(value1[key], value2[key], level + 4);
-        const diffParams = [{
-          key, value, level, char: ' ',
-        }];
-        return format(formatName, diffParams);
+        const value = compareData(value1[key], value2[key], level + 4, [...way, key]);
+        return {
+          key, value, status: 'root', level, way,
+        };
       }
-      const diffParams = genLineDiff(key, value1, value2, level);
-      return format(formatName, diffParams);
+      return { ...getLineDiff(key, value1, value2), way: [...way, key], level };
     });
-
-    const diff = `{\n${diffArr.join('\n')}\n${' '.repeat(level - 2)}}`;
-    return diff;
+    return diffParams;
   };
-  return compareData(data1, data2);
+
+  return format(formatName, compareData(data1, data2));
 };
 
 export { readFile };
