e3090eea63fca180a3b170300a5e4c89c9e3feaf
Ekaterina <brekhova.katya@gmail.com>
2022-04-06 14:11:35 +0300

    Add ability to get diff as Plain text

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -3,7 +3,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const complexDataDiff = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}';
+const stylishDiff = '{\n    common: {\n      + follow: false\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: null\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n        setting6: {\n            doge: {\n              - wow: \n              + wow: so much\n            }\n            key: value\n          + ops: vops\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n        deep: {\n            id: 45\n        }\n    }\n  + group3: {\n        deep: {\n            id: {\n                number: 45\n            }\n        }\n        fee: 100500\n    }\n}';
+const plainDiff = "Property 'common.follow' was added with value: false\nProperty 'common.setting2' was removed\nProperty 'common.setting3' was updated. From true to null\nProperty 'common.setting4' was added with value: 'blah blah'\nProperty 'common.setting5' was added with value: [complex value]\nProperty 'common.setting6.doge.wow' was updated. From '' to 'so much'\nProperty 'common.setting6.ops' was added with value: 'vops'\nProperty 'group1.baz' was updated. From 'bas' to 'bars'\nProperty 'group1.nest' was updated. From [complex value] to 'str'\nProperty 'group2' was removed\nProperty 'group3' was added with value: [complex value]";
 
 let complexJson1;
 let complexJson2;
@@ -21,6 +22,6 @@ beforeAll(() => {
 });
 
 test('test genDiff() with complex data', () => {
-  expect(genDiff(complexJson1, complexJson2)).toEqual(complexDataDiff);
-  expect(genDiff(complexYaml1, complexYaml2)).toEqual(complexDataDiff);
+  expect(genDiff(complexJson1, complexJson2)).toEqual(stylishDiff);
+  expect(genDiff(complexYaml1, complexYaml2, 'plain')).toEqual(plainDiff);
 });
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,43 @@
-const plain = () => {
+import isObject from '../utilitys.js';
 
+const plainValue = (value) => {
+  if (isObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const plainLine = ({
+  from, to, status, way,
+}) => {
+  switch (status) {
+    case 'added':
+      return `Property '${way.join('.')}' was added with value: ${plainValue(to)}`;
+    case 'removed':
+      return `Property '${way.join('.')}' was removed`;
+    case 'updated':
+      return `Property '${way.join('.')}' was updated. From ${plainValue(from)} to ${plainValue(to)}`;
+    default:
+      return null;
+  }
+};
+
+const plain = (params) => {
+  const inner = (node) => {
+    if (node.status === 'root') {
+      return node.value.flatMap(inner);
+    }
+    return plainLine(node);
+  };
+  const res = params
+    .flatMap(inner)
+    .filter((item) => item !== null)
+    .join('\n');
+
+  return `${res}`;
 };
 
 export default plain;
